d555e5d128d2bcfcb8a8215eea5ed7836e4c22ab
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2024-04-18 20:17:30 +0530

    Update main.py

--- Extract the ip address of the hostname /main.py
+++ Extract the ip address of the hostname /main.py
@@ -8,3 +8,10 @@ if __name__ == '__main__':
         print(f'The ip address of the host {hostname} is: {addr}')
     except socket.gaierror as e:
         print(f"Error: Cannot resolve hostname {hostname}. Please enter a valid hostname.")
+
+
+
+# here 
+# __name__ -> it is a special variable that stores the name of the current module or script
+# In the context of the code snippet you provided, if __name__ == '__main__': is a common idiom used to check if the Python script is being run directly by the interpreter. If the script is being executed directly, the __name__ variable will be set to '__main__'. This is useful when you have code that you only want to run when the script is executed directly, not when it is imported into another script.
+# So, by checking if __name__ == '__main__':, you ensure that the code block underneath it will only run when the Python script is directly executed, not when it is imported into another script.
