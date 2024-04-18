c8d37002ca92a4acc38163243e7728b153de572d
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2024-04-18 20:11:50 +0530

    Update main.py

--- Extract the ip address of the hostname /main.py
+++ Extract the ip address of the hostname /main.py
@@ -1,8 +1,10 @@
 
-
 import socket
 
 if __name__ == '__main__':
-   hostname = 'maps.google.com'
-   addr = socket.gethostbyname(hostname)
-   print(f'The ip address of the host {hostname} is :{addr}')
+    hostname = input("Enter the hostname: ")
+    try:
+        addr = socket.gethostbyname(hostname)
+        print(f'The ip address of the host {hostname} is: {addr}')
+    except socket.gaierror as e:
+        print(f"Error: Cannot resolve hostname {hostname}. Please enter a valid hostname.")
