fbeb2924494aaa4d27a58ca817a30dd8385cbb79
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2024-04-22 23:44:04 +0530

    Create undirectedgraph(labelled).py

---
+++ NetworkX Package/undirectedgraph(labelled).py
@@ -0,0 +1,15 @@
+import networkx as nx
+# importing matplotlib.pyplot
+import matplotlib.pyplot as plt
+ 
+g = nx.Graph()
+d=nx.Graph()
+ 
+g.add_edge(1, 2)
+g.add_edge(2, 3)
+g.add_edge(3, 4)
+g.add_edge(1, 4)
+g.add_edge(1, 5)
+ 
+nx.draw(g,with_labels=True)
+plt.savefig("unlabledgraph.png")
